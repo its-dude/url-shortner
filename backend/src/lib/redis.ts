36945db4cfdfa1d5483287b967d732bb6eeeb215
Abhishek Mishra <abhim5920@gmail.com>
2026-01-25 15:53:08 +0000

    chore: add Dockerfile and docker-compose setup

--- backend/src/lib/redis.ts
+++ backend/src/lib/redis.ts
@@ -1,33 +1,43 @@
-import { createClient, RedisClientType } from "redis"
+import { createClient, RedisClientType} from "redis"
 import { config } from "../config/config"
 
 let redisClient: RedisClientType | null = null
 
 function getRedisClient(): RedisClientType {
-    if (!redisClient){
-        redisClient = createClient({url: config.redisUrl})
+    if (!redisClient) {
+        redisClient = createClient({
+    url: config.redisUrl,
+    socket: {
+        reconnectStrategy: (retries: number): number | false | Error=> {
+            if (retries > 10) return new Error("Redis retry limit reached");
+            return Math.min(retries * 500, 3000);
+        },
+    },
+});
+
+
 
         redisClient.on('error', (err) => {
             console.log("Redis error: ", err.message)
         })
 
         redisClient.connect()
-        .then(() => {
-            console.log("Redis db connected")
-        })
-        .catch((err)=>{
-            console.error("Redis connection failed: ", err.message)
-        })
+            .then(() => {
+                console.log("Redis db connected")
+            })
+            .catch((err) => {
+                console.error("Redis connection failed: ", err)
+            })
     }
 
     return redisClient
 }
 
-async function diconnectRedis(): Promise<void>{
+async function diconnectRedis(): Promise<void> {
     if (redisClient && redisClient.isOpen) {
         await redisClient.quit();
         redisClient = null;
     }
 }
 
-export {diconnectRedis, getRedisClient}
+export { diconnectRedis, getRedisClient }
